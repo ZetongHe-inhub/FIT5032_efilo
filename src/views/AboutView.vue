0d74f59000a7c4533106862f951597d2d4d93a9a
zetongh1 <zetong.he1@student.unimelb.edu.au>
2025-09-05 16:28:13 +1000

    efilo week5

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">About Our Library</h1>
+        <p class="text-center">
+          Welcome to our digital library! We're dedicated to providing a vast collection of books
+          and resources to our community.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// no script needed
+</script>
+
+<style scoped>
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container mt-5">
+    <h1>Login</h1>
+        <form @submit.prevent="submitForm">
+            <!-- Username + Gender -->
+            <div class="row mb-3">
+              <div class="col-md-6 col-sm-6">
+                <label for="username" class="form-label">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  @blur="() => validateName(true)"
+                  @input="() => validateName(false)"
+                  v-model="formData.username"
+                />
+                <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
+              </div>
+
+              <div class="col-md-6 col-sm-6">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  @blur="() => validatePassword(true)"
+                  @input="() => validatePassword(false)"
+                  v-model="formData.password"
+                />
+                <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+              </div>
+            </div>
+            
+        </form>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+const submitForm = () => {
+  validateName(true)
+  validatePassword(true)
+  validateConfirmPassword(true)
+  if (!errors.value.username && !errors.value.password && !errors.value.confirmPassword) {
+    submittedCards.value.push({ ...formData.value })
+    clearForm()
+  }
+}
+</script>
+
+<style scoped>
+</style>
